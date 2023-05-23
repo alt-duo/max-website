a4ab33f48407c88f7a4fef40f9c8cb2007e2ebc1
Max <maxlovesairandteslas@gmail.com>
2023-05-23 16:54:10 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  dark: 'class',
+  content: ['./build/*.html'],
+  theme: {
+    extend: {},
+    colors: {
+      'main': '#596886',
+      'main1': '#052b4a',
+      'main2': '#ffd5af',
+      'main3': '#ccac8f',
+    },
+  },
+  plugins: [],
+}
+
